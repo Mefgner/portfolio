005183e0d5f405a4938f2054a5bb5a38350b082c
Yevhenii Prasol <mefgner@gmail.com>
2025-05-17 15:26:36 +0200

    added support for config in db class

--- classes/db.class.php
+++ classes/db.class.php
@@ -7,7 +7,30 @@ class Database
     protected $db = null;
     public function __construct($db)
     {
-        $this->db = $this->connect($db);
+        if (is_null($db) || $db == "" || empty($db)) {
+            throw new \Exception("Database name cannot be null");
+        }
+
+        $file = file_get_contents("db_config.json");
+        $json = json_decode($file, true);
+
+        if (isset($json['host'])) {
+            $host = $json['host'];
+        } else {
+            $host = "localhost";
+        }
+        if (isset($json['user'])) {
+            $user = $json['user'];
+        } else {
+            $user = "root";
+        }
+        if (isset($json['password'])) {
+            $password = $json['password'];
+        } else {
+            $password = "";
+        }
+
+        $this->db = $this->connect($db, $host, $user, $password);
     }
     private function connect($db, $host = "localhost", $user = "root", $password = "")
     {
